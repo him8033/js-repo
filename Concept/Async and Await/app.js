9b703fe0f5b6af88a63b9943a9200fe08ddb4c49
him8033 <himanshu8033@gmail.com>
2024-11-14 11:30:11 +0530

    asynce await function defines

---
+++ Concept/Async and Await/app.js
@@ -0,0 +1,21 @@
+let h1= document.querySelector("h1");
+
+function changeColor(color,delay){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            h1.style.color=color;
+            console.log(`color changed to ${color}`);
+            resolve("color changed successfully");
+        },delay);
+    })
+}
+
+async function demo() {
+    await changeColor("red",1000);
+    await changeColor("green",1000);
+    await changeColor("blue",1000);
+    await changeColor("yellow",1000);
+    await changeColor("pink",1000);
+}
+
+demo();
